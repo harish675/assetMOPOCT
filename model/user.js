bb5b5320c59c918cfc3040de46ea1b8912371703
Harish Jadhav <harishjadhav675@gmail.com>
2023-09-01 10:10:47 +0530

    created user model

---
+++ model/user.js
@@ -0,0 +1,36 @@
+
+const mongoose = require('mongoose');
+
+//creating user Schema for storing users details
+
+const userSchema = new mongoose.Schema({
+     
+      email:{
+         
+         type:String,
+         required:true,
+         unique:true,
+       },
+     password :{
+          
+          type:String,
+          required:true,
+     },
+     name:{
+         type:String,
+         required:true,
+     },
+     gender :{
+         type:String,
+         required:true,
+     }
+     
+},{
+    
+    timestamps:true,
+})
+
+
+const User = mongoose.model('User',userSchema);
+
+module.exports = User;

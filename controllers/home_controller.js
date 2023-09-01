97457a1e0bdf96e4982b1095421a1e132614b8bd
Harish Jadhav <harishjadhav675@gmail.com>
2023-09-01 09:00:58 +0530

    added basic routes and controllers

---
+++ controllers/home_controller.js
@@ -0,0 +1,7 @@
+
+module.exports.homePage = function(req,res){
+     
+      
+      return res.send('Well come to home page');
+     
+}
--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const app = express();
 const port = 8000;
 
 
+app.use('/',require('./routes'));
+
+
 
 //check server is running or not
-
-app.listen(port,function(err){
-     
+app.listen(port,function(err){ 
      if(err){
           console.log("Error in Running the Server");
      }
-
      console.log("Server is running on port ",port);
 })

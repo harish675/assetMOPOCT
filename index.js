691975af08f3672f9683e4056b0627d6a225ebd8
Harish Jadhav <harishjadhav675@gmail.com>
2023-09-01 08:35:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+const app = express();
+const port = 8000;
+
+
+
+//check server is running or not
+
+app.listen(port,function(err){
+     
+     if(err){
+          console.log("Error in Running the Server");
+     }
+
+     console.log("Server is running on port ",port);
+})
